8d6c6c38dd8444e826f1f2a5cecd0586cd7fe998
Balázsi Levente <balazsi.levente@hallgato.ppke.hu>
2020-04-23 15:25:49 +0200

    [VIR-17] Setup campus locations database.

--- model/core.py
+++ model/core.py
@@ -2,6 +2,7 @@ from model.location import Location
 from view.main_menu_view import MainMenuView
 from view.scene_3d_view import Scene3DView
 from view.pause_menu_view import PauseMenuView
+import csv
 
 
 class Core:
@@ -20,10 +21,9 @@ class Core:
         with open(locations_file, "r") as l_file:
             data = csv.DictReader(l_file, delimiter="|")
             for row in data:
-                split_c = [int(i) for i in row["map_coord"].split(',')]
-                split_n = row["neighbors"].split(',')
-                print(row["id"], row["texture"], split_c, split_n)
-                current_location = Location(id=row["id"], neighbors=split_n, texture=row["texture"])
+                split_coord = [int(i) for i in row["map_coord"].split(',')]
+                split_neighbors = row["neighbors"].split(',')
+                current_location = Location(id=row["id"], neighbors=split_neighbors, texture=row["texture"])
                 self.locations.append(current_location)
 
     def run_program(self):
