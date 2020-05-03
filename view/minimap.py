c6cce0e3a2c049a231317844d9719ded947f68f6
Balázsi Levente <balazsi.levente@hallgato.ppke.hu>
2020-05-03 17:36:39 +0200

    [VIR-10] Design topological map of campus

--- view/minimap.py
+++ view/minimap.py
@@ -8,11 +8,17 @@ class Minimap:
         self.core = core
         self.image = OnscreenImage(image="resource/minimap.png", pos=(0.7, 0, -0.7), parent=self.core.render2d, scale=0.2)
         #self.image = OnscreenText(text='anyád', pos=(0, 0), scale=0.5)
+
+        #self.line_node.create()
+
+        x = self.core.locations[0].map_coord[0]
+        y = self.core.locations[0].map_coord[1]
+
         linesegs = LineSegs()
         linesegs.set_color(1, 0, 1, 0)
-        linesegs.move_to(0.0, 0.0, 0.0)
+        linesegs.move_to(x, 0.0, y)
         linesegs.draw_to(0.5, 0.0, 0.5)
         line_node = linesegs.create()
         self.image.attachNewNode(line_node)
-        #self.line_node.create()
+
 
