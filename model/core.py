4528eb19cccd76788ad0c0658f4285a062625149
mblazsek <martin.blazsek@gmail.com>
2020-05-10 09:45:36 +0200

    [VIR-35] Add location markers

--- model/core.py
+++ model/core.py
@@ -15,12 +15,16 @@ import csv
 class Core(ShowBase, DirectObject):
     WINDOW_WIDTH = 800
     WINDOW_HEIGHT = 600
+    ASPECT_RATIO = WINDOW_WIDTH / WINDOW_HEIGHT
     REFERENCE_ANGLE = 188
     PATHS = {
-        "3D_SCENE_MODEL": "resource/cylinder.egg",
+        "3D_SCENE_MODEL": "resource/models/cylinder.egg",
         "LOCATIONS_DB": "resource/location_file.txt",
-        "MINIMAP_BG": "resource/minimap.png",
-        "MINIMAP_POINT_MODEL": "resource/point.egg"
+        "MINIMAP_BG_TEXTURE": "resource/textures/minimap.png",
+        "MINIMAP_BG_MODEL": "resource/models/minimap.egg",
+        "MINIMAP_POINT_MODEL": "resource/models/point.egg",
+        "LOCATION_MARKER_MODEL": "resource/models/arrow.egg",
+        "LOCATION_MARKER_TEXTURE": "resource/textures/arrow.png"
     }
 
     def __init__(self):
@@ -35,6 +39,7 @@ class Core(ShowBase, DirectObject):
         # load data
         self.load_data()
         self.set_reference_point()
+        self.set_neighbor_markers()
         self.active_location = self.locations[0]
 
         # define views
@@ -57,12 +62,12 @@ class Core(ShowBase, DirectObject):
         def process_coords():
             split_coords = row["map_coord"].split(',')
             map_x, map_y = [int(i) for i in split_coords]
-            map_x_normed = ((map_x*2) / self.WINDOW_WIDTH) - 1
-            map_y_normed = -(((map_y*2) / self.WINDOW_HEIGHT) - 1)
+            map_x_normed = ((map_x*2) / 549) - 1
+            map_y_normed = -(((map_y*2) / 549) - 1)
             return map_x_normed, map_y_normed
 
         def process_texture():
-            texture_path = Path("resource/{}".format(row["texture"]))
+            texture_path = Path("resource/textures/{}".format(row["texture"]))
             texture = self.loader.loadTexture(texture_path)
             return texture
 
@@ -70,18 +75,18 @@ class Core(ShowBase, DirectObject):
         with open(self.PATHS["LOCATIONS_DB"], "r") as l_file:
             data = csv.DictReader(l_file, delimiter="|")
             for row in data:
-                id = row["id"]
+                id = int(row["id"])
                 x, y = process_coords()
-                neighbors = row["neighbors"].split(',')
+                neighbors = [int(neighbor_id) for neighbor_id in row["neighbors"].split(',')]
                 texture = process_texture()
                 location = Location(id, x, y, neighbors, texture)
                 location.reparentTo(self.render2d)
                 self.locations.append(location)
 
     @staticmethod
-    def calculate_displacement(origin, target, transpose=False):
-        origin_x, origin_y = origin.get_position()
-        target_x, target_y = target.get_position()
+    def calculate_displacement(origin_pos, target_pos, transpose=False):
+        origin_x, origin_y = origin_pos
+        target_x, target_y = target_pos
         if transpose:
             return np.array([[target_x - origin_x, target_y - origin_y]]).T
         else:
@@ -89,20 +94,58 @@ class Core(ShowBase, DirectObject):
 
     def set_reference_point(self):
         theta = 2*math.pi-math.radians(self.REFERENCE_ANGLE)
-        origin = self.locations[0]
-        target = self.locations[1]
-        v = self.calculate_displacement(origin, target, transpose=True)
+        origin_pos = self.locations[0].get_position()
+        target_pos = self.locations[1].get_position()
+        v = self.calculate_displacement(origin_pos, target_pos, transpose=True)
         v_norm = math.sqrt(v[0]**2+v[1]**2)
         rotation_matrix = np.matrix([[math.cos(theta), -math.sin(theta)],
                                      [math.sin(theta),  math.cos(theta)]])
-        offset_x, offset_y = origin.get_position()
-        self.reference_point = np.array([offset_x, offset_y])+np.transpose((1/v_norm)*rotation_matrix*v)
-        pass
+        offset_x, offset_y = origin_pos
+        reference_point_matrix = np.array([offset_x, offset_y])+np.transpose((1/v_norm)*rotation_matrix*v)
+        self.reference_point = reference_point_matrix.tolist()[0]
+
+    def set_neighbor_markers(self):
+        marker_texture_path = self.PATHS["MINIMAP_BG_TEXTURE"]
+        marker_texture = self.loader.loadTexture(marker_texture_path)
+        for location in self.locations:
+            location_pos = location.get_position()
+            for neighbor_id in location.get_neighbors():
+                neighbor = self.find_location_by_id(neighbor_id)
+                neighbor_pos = neighbor.get_position()
+                neighbor_displaced = self.calculate_displacement(location_pos, neighbor_pos)
+                neighbor_displaced_x, neighbor_displaced_y = neighbor_displaced.tolist()
+
+                reference_displaced = self.calculate_displacement(location_pos, self.reference_point)
+                reference_displaced_x, reference_displaced_y = reference_displaced.tolist()
+
+                dot_product = neighbor_displaced_x * reference_displaced_x + neighbor_displaced_y * reference_displaced_y
+                displacement_norm = math.sqrt(neighbor_displaced_x ** 2 + neighbor_displaced_y ** 2)
+                reference_norm = math.sqrt(reference_displaced_x ** 2 + reference_displaced_y ** 2)
+                angle = math.degrees(math.acos(dot_product / (displacement_norm * reference_norm)))
+
+                def reference_line(x_pos):
+                    slope = reference_displaced_y / reference_displaced_x
+                    return slope * x_pos
+
+                if reference_line(neighbor_displaced_x) > neighbor_displaced_y:
+                    angle = 360-angle
+
+                location.add_neighbor_marker(neighbor, angle, marker_texture)
 
     def find_location_by_id(self, id):
         for location in self.locations:
             if location.id == id:
                 return location
+        return None
+
+    def find_location_by_marker(self, marker):
+        for location in self.locations:
+            for neighbor_id in location.get_markers():
+                neighbor = self.find_location_by_id(neighbor_id)
+                _, loc_marker = location.get_markers()[neighbor_id]
+                if marker == loc_marker:
+                    return neighbor
+                print(location.id)
         return None
 
     def get_active_view(self):
@@ -117,6 +160,8 @@ class Core(ShowBase, DirectObject):
 
     def set_active_location(self, new_location):
         old_location = self.active_location
+        old_location_pos = old_location.get_position()
+        new_location_pos = new_location.get_position()
         for location in self.locations:
             if location is new_location:
                 location.set_to_active()
@@ -125,8 +170,4 @@ class Core(ShowBase, DirectObject):
                 location.set_to_inactive()
         texture = self.active_location.get_texture()
         self.scene_3d_model.setTexture(texture)
-        print(old_location.get_position())
-        print(new_location.get_position())
-        print(self.reference_point)
-        self.reference_point += self.calculate_displacement(old_location, new_location)
-        print(self.reference_point)
+        self.reference_point += self.calculate_displacement(old_location_pos, new_location_pos)
--- view/minimap.py
+++ view/minimap.py
@@ -1,6 +1,8 @@
-from direct.gui.OnscreenImage import OnscreenImage
 from direct.gui.DirectFrame import DirectFrame
 from panda3d.core import LineSegs
+from panda3d.core import TransparencyAttrib
+from direct.actor.Actor import Actor
+from panda3d.core import CardMaker
 
 
 class Minimap:
@@ -11,27 +13,41 @@ class Minimap:
     def __init__(self, core):
         self.core = core
         self.locations = self.core.locations
-        self.screen = DirectFrame(frameColor=(0, 0, 0, 1), parent=self.core.render2d)
+        scaling_vector = (self.MINIMAP_SCALE/self.core.ASPECT_RATIO, 0, self.MINIMAP_SCALE)
+        self.screen = DirectFrame(frameColor=(0, 0, 0, 0), pos=self.MINIMAP_POSITION,
+                                  scale=scaling_vector, parent=self.core.render2d)
         self.map = None
-
         self.draw_background()
         self.draw_lines()
         self.draw_points()
         self.screen.hide()
 
     def draw_background(self):
-        background_path = self.core.PATHS["MINIMAP_BG"]
-        position = self.MINIMAP_POSITION
-        scale = self.MINIMAP_SCALE
-        self.map = OnscreenImage(image=background_path, pos=position, scale=scale, parent=self.screen)
+        background_model_path = self.core.PATHS["MINIMAP_BG_MODEL"]
+        background_texture_path = self.core.PATHS["MINIMAP_BG_TEXTURE"]
+        self.map = Actor(background_model_path)
+        texture = self.core.loader.loadTexture(background_texture_path)
+        self.map.setColor(1, 1, 1, 1)
+        self.map.setTexture(texture)
+        self.map.reparentTo(self.screen)
 
     def draw_points(self):
         for location in self.locations:
-            location.reparentTo(self.map)
+            location.reparentTo(self.screen)
+            location.setBin("fixed", 0)
+            location.setDepthTest(False)
+            location.setDepthWrite(False)
 
     def draw_lines(self):
+        cm = CardMaker('lines_background_node')
+        cm.setFrame(-1, 1, -1, 1)
+        cm_node = self.screen.attachNewNode(cm.generate())
+        cm_node.setTransparency(TransparencyAttrib.MAlpha)
+        cm_node.setBin('fixed', 0)
+        cm_node.setColor(0, 0, 0, 0)
+
         linesegs = LineSegs()
-        linesegs.setColor(0.0, 0.0, 0.0, 0.0)
+        linesegs.setColor(0.0, 0.0, 0.0, 1.0)
         linesegs.setThickness(2.25)
         for location in self.locations:
             loc_x, loc_y = location.get_position()
@@ -42,7 +58,7 @@ class Minimap:
                 linesegs.drawTo(nb_x, 0.0, nb_y)
                 linesegs.moveTo(loc_x, 0.0, loc_y)
         line_node = linesegs.create()
-        self.map.attachNewNode(line_node)
+        cm_node.attachNewNode(line_node)
 
     def show(self):
         self.screen.show()
